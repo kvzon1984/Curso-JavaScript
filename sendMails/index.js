ade74c95202c09799b9736b98d7ce4d5878c263d
kvzon1984 <ivega.josue@gmail.com>
2023-06-23 13:55:00 -0400

    fix: lista animales send mails

---
+++ sendMails/index.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import path from 'path';
+
+const app = express()
+
+app.use(express.json())
+app.use(express.static('app'))
+
+app.get('/', (req, res) => {
+    res.sendFile(`${path.resolve()}/index.html`)
+})
+
+app.listen(3000, () => console.log('La app esta corriendo!'))
